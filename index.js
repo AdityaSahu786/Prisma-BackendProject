caa9211eee316f350f69c0aa7072a81a5a917528
AdityaSahu786 <sahuadi786183@gmail.com>
2024-05-30 08:56:30 +0530

    completed setting up the prisma project

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const cookieParser = require('cookie-parser')
+const express = require('express')
 
+require('dotenv').config()
 const app = express();
+
+// regular middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// cookies middleware
+app.use(cookieParser())
+
+const userRouter = require('./routes/userRoutes')
+
+app.use('/api', userRouter)
 
 app.get('/', (req, res) => {
     res.send("Hi from youtube live")
